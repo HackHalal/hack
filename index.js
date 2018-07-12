1fad1ac592238a111c1f1689f74f486d71e34853
Abdul Ghaffar <aghaffar570@gmail.com>
2018-07-11 22:00:08 -0400

    new layout

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-let header = document.getElementsByTagName('header')[0];
+// let header = document.getElementsByTagName('header')[0];
 
-let scrolling = true;
+// let scrolling = true;
 
-const showHeader = 60;
-const showMockup = 250;
+// const showHeader = 60;
+// const showMockup = 250;
 
-const scrollAnimation = () => {
-  const sections = Array.from(document.getElementsByTagName('section'));
-  const rects = sections.map(section => section.getBoundingClientRect());
+// const scrollAnimation = () => {
+//   const sections = Array.from(document.getElementsByTagName('section'));
+//   const rects = sections.map(section => section.getBoundingClientRect());
 
-  // set if header is visible
-  if (window.scrollY > showHeader) {
-    header.classList.add('active');
-  } else {
-    header.classList.remove('active');
-  }
-  // set if client is visible
-  if (window.scrollY > showMockup) {
-    mockup.classList.add('active');
-  } else {
-    mockup.classList.remove('active');
-  }
+//   console.log(window.scrollY, window.innerHeight);
+//   // set if header is visible
+//   if (window.scrollY > 300) {
+//     console.log('pop now');
+//   }
 
-  for (let i = 0; i < rects.length; i++) {
-    let section = sections[i];
-    let rect = rects[i];
+//   if (window.scrollY > showHeader) {
+//     header.classList.add('active');
+//   } else {
+//     header.classList.remove('active');
+//   }
+//   // set if client is visible
+//   if (window.scrollY > showMockup) {
+//     mockup.classList.add('active');
+//   } else {
+//     mockup.classList.remove('active');
+//   }
 
-    if (rect.top < window.innerHeight && rect.bottom > 0) {
-      section.classList.add('active');
-    } else {
-      section.classList.remove('active');
-    }
-  }
-  scrolling = false;
-};
+//   for (let i = 0; i < rects.length; i++) {
+//     let section = sections[i];
+//     let rect = rects[i];
 
-window.addEventListener('scroll', () => {
-  if (!scrolling) {
-    // prevent layout thrashing
-    window.requestAnimationFrame(scrollAnimation);
-    scrolling = true;
-  }
-});
+//     if (rect.top < window.innerHeight && rect.bottom > 0) {
+//       section.classList.add('active');
+//     } else {
+//       section.classList.remove('active');
+//     }
+//   }
+//   scrolling = false;
+// };
 
-scrollAnimation();
+// window.addEventListener('scroll', () => {
+//   if (!scrolling) {
+//     // prevent layout thrashing
+//     window.requestAnimationFrame(scrollAnimation);
+//     scrolling = true;
+//   }
+// });
+
+// scrollAnimation();
